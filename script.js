3daceef9fb2a3d94563cc6b71114bd6d23422db0
名前 <>
2024-08-28 12:05:05 +0900

    引き算に変更

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculate() {
   const num2 = parseFloat(document.getElementById("num2").value);
 
   //計算処理
-  const result = num1 + num2;
+  const result = num1 - num2;
 
   document.getElementById("result").textContent = `計算結果: ${result}`;
 }
