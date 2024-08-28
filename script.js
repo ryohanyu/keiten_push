72ece5f9c010b4904a8ec9ec2c487063f4859ffc
名前 <>
2024-08-28 11:51:44 +0900

    コミット

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+
+  //計算処理
+  const result = num1 + num2;
+
+  document.getElementById("result").textContent = `計算結果: ${result}`;
+}
